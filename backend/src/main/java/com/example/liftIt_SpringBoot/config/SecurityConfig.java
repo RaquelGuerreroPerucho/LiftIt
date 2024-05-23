e0ed87cb52215ddb205050d6a0f51b8583afc383
Raquel Guerrero Perucho <raquelguerreroperucho@gmail.com>
2024-05-23 11:20:50 +0200

    login y register
    login y register funcionando

--- backend/src/main/java/com/example/liftIt_SpringBoot/config/SecurityConfig.java
+++ backend/src/main/java/com/example/liftIt_SpringBoot/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -38,6 +39,8 @@ public class SecurityConfig {
                 )
                 .authenticationProvider(authProviver)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .build();
 
     }
--- frontend/liftIt/src/app/inicio/inicio.page.ts
+++ frontend/liftIt/src/app/inicio/inicio.page.ts
@@ -1,15 +1,77 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../services/usuario.service';
+import { NavController } from '@ionic/angular';
+
+
+import { catchError, tap } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
+export class InicioPage{
 
-  constructor() { }
+  nombreUsuario : string = "";
+  email : string = "";
+  contrasenya : string = "";
+  repetirContasenya : string = "";
 
-  ngOnInit() {
+  emailLogin : string = "";
+  contrasenyaLogin : string = "";
+
+  userSev : UsuarioService;
+
+  constructor(private usuarioService : UsuarioService, private navCtrl : NavController){
+
+    this.userSev = usuarioService;
+    
+   }
+
+  registrarse(){
+    console.log(this.nombreUsuario);
+    console.log(this.email);
+    console.log(this.contrasenya);
+    console.log(this.repetirContasenya);
+
+    this.userSev.registerUser(this.email, this.contrasenya).pipe(
+      tap(result => {
+        console.log(result);
+      }),
+      catchError(err => {
+        console.error(err);
+        return of(err);
+      })
+    ).subscribe();
+/*    
+    this.userSev.registerUser(this.email, this.contrasenya).subscribe(
+      result => {
+        console.log(result);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+    */
+  }
+
+  login(){
+    
+    console.log(this.emailLogin);
+    console.log(this.contrasenyaLogin);
+
+    this.userSev.loginUser(this.emailLogin, this.contrasenyaLogin).pipe(
+      tap(result => {
+        console.log(result);
+        this.navCtrl.navigateForward('calendario');
+      }),
+      catchError(err => {
+        console.error(err);
+        return of(err);
+      })
+    ).subscribe();
   }
 
   selectTabs = 'registrarse';
