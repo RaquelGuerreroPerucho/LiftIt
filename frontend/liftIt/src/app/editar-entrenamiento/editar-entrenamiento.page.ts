9034a2cb89b2f6af144d9395c96df367b11165eb
Raquel Guerrero Perucho <raquelguerreroperucho@gmail.com>
2024-05-31 20:36:31 +0200

    mas cosas del dia
    muchas cosas distintas la verdad

--- frontend/liftIt/src/app/editar-entrenamiento/editar-entrenamiento.page.ts
+++ frontend/liftIt/src/app/editar-entrenamiento/editar-entrenamiento.page.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editar-entrenamiento.page.scss'],
 })
 export class EditarEntrenamientoPage implements OnInit {
+trainingTime: string = '';
+intensidad: string = '';
+sensaciones: string = '';
+calentamiento: string = '';
+ejerciciosPrincipales: string = '';
+ejerciciosComplementarios: string = '';
+core: string = '';
+enfriamiento: string = '';
+esTiempoValido: boolean = true;
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  validarTiempo() {
+    const regex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/;
+    this.esTiempoValido = regex.test(this.trainingTime);
   }
 
 }
--- frontend/liftIt/src/app/nuevo-entrenamiento/nuevo-entrenamiento.page.ts
+++ frontend/liftIt/src/app/nuevo-entrenamiento/nuevo-entrenamiento.page.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nuevo-entrenamiento.page.scss'],
 })
 export class NuevoEntrenamientoPage implements OnInit {
+  sensaciones: string = '';
+  calentamiento: string = '';
+  ejerciciosPrincipales: string = '';
+  ejerciciosComplementarios: string = '';
+  core: string = '';
+  enfriamiento: string = '';
+  trainingTime: string = '';
+  esTiempoValido: boolean = true;
+  intensidad: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  validarTiempo() {
+    const regex = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/;
+    this.esTiempoValido = regex.test(this.trainingTime);
   }
-
 }
